e8b5ed00772167a763edf2b0d8085cbc3e2fed1f
abbv75 <bore.younous59@gmail.com>
2026-05-19 15:15:55 +0000

    feat: create geoserver_layers migration table

---
+++ database/migrations/2026_05_19_151300_create_geoserver_layers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('geoserver_layers', function (Blueprint $table) {
+            $table->string('id')->primary();
+
+            // Type du layer (point, polygon, line, raster…)
+            $table->string('type');
+
+            $table->string('title');
+
+            $table->string('name');
+            $table->text('openlayerUrl');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('geoserver_layers');
+    }
+};
